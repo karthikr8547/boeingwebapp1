f85466eac81f3bde6ee3fb290453221afca6a1df
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-10 21:01:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//var http = require('http');
-var express = require('express');
-var app = express();
+var http = require('http');
 
-app.get('/',function(req,res)
-{
-res.send('Hello World!');
-});
-var server=app.listen(8081,function() {});
 
-//http.createServer(function (req, res) {
+http.createServer(function (req, res) {
     
- //   res.writeHead(200, { 'Content-Type': 'text/html' });
- //   res.end('Hello, world!');
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+   res.end('Hello, world!');
     
-//}).listen(process.env.PORT || 8080);
+}).listen(process.env.PORT || 8080);

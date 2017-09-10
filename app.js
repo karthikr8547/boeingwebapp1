bfb852d7d001ff3e85921b6f18c921960f437d8e
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-10 21:29:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/',function(req,res){
   res.send("Hello Express App");
 });
 
-app.listen(3000,function(){
+var port = process.env.PORT || 1337;
+
+app.listen(port,function(){
 	console.log("Server started......");
 });

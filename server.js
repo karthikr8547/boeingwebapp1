928b53319fdc1cff613c61e089f92b14d727e863
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-10 17:49:58 +0530

    port no changed to 8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/',function(req,res)
 {
 res.send('Hello World!');
 });
-var server=app.listen(3000,function() {});
+var server=app.listen(8080,function() {});
 
 //http.createServer(function (req, res) {
     

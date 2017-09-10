beb107fe6a9e6302e3cc650ceefbf8f9ada7258a
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-10 21:57:41 +0530

    file upload

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ app.get('/',function(req,res){
   res.send("Hello Express App");
 });
 
+app.get('/upload', function (req, res) {
+    res.send(
+    '<form action="/upload" method="post" enctype="multipart/form-data">' +
+    '<input type="file" name="snapshot" />' +
+    '<input type="submit" value="Upload" />' +
+    '</form>'
+    );
+});
+
+app.post('/upload', function (req, res) {
+    var path = req.files.snapshot.path;
+    var bs= azure.createBlobService();
+    bs.createBlockBlobFromFile('c', 'test.png', path, function (error) { });
+    res.send("OK");
+});
+
 var port = process.env.PORT || 1337;
 
 app.listen(port,function(){

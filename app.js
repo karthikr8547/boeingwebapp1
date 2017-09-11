893620790aa4694d664ea07ca0ba8e7dc4302e6f
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-11 20:32:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/upload', function (req, res) {
         }
     });
     form.parse(req);
-    res.writeHead(200, {'content-type': 'text/plain'});
+    res.setHeader('content-type', 'text/plain');
     res.send('OK');
 });	    
 	    

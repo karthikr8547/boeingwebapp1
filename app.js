ade7e16039f601afe98f95687feb399c5f86cd28
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-12 20:51:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -59,8 +59,22 @@ app.get('/upload', function (req, res) {
     res.send("OK");
 });*/
 
+function uploadFile(req,res,next){
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        //fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+	fstream = fs.createWriteStream(__dirname + '/' + filename);
+	file.pipe(fstream); 
+	fstream.on('close', function () {	
+	next();
+	// res.redirect('back');
+	});
+    });
+}
 
-app.post('/upload', function (req, res) {
+app.post('/upload', uploadFile, function (req, res) {
     var blobService = azure.createBlobService('boeingwepapp1','YqMF4F3rl76F/IhcRUXj1Ede1zHlSRHCtly/7BjB1cMAjsMBlksK3O8DPwFlIy0PfU/TiPBEDdvXGahZeeH4tQ==');  
     var form = new multiparty.Form();
 	form.on('part', function(part) {
@@ -82,23 +96,6 @@ app.post('/upload', function (req, res) {
     res.setHeader('content-type', 'text/plain');
     res.send('OK');
 });
-
-/*function uploadFile(req,res,next){
-    var fstream;
-    req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("Uploading: " + filename); 
-        //fstream = fs.createWriteStream(__dirname + '/files/' + filename);
-	fstream = fs.createWriteStream(__dirname + '/' + filename);
-	file.pipe(fstream); 
-	fstream.on('close', function () {	
-        res.redirect('back');
-	});
-    });
-	next();
-}*/
-
-
 
 /*app.post('/upload', function(req, res) {
     var fstream;

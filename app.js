c97f35b0ced2316ee630b13b64c72142daee41cd
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-12 18:30:24 +0530

    file server upload

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 /* global azure */
 //Initial draft
 var express = require('express');
-var fileUpload = require('express-fileupload');
-var app = express();
+var http = require('http'),
+var formidable = require('formidable'),
+var fs = require('fs'),
+var path = require('path');
 var azure = require('azure-storage');
 var formidable = require('formidable');
 var multiparty = require('multiparty');
+
+var app = express();
 
 //app.use(fileUpload());
 
@@ -51,6 +55,7 @@ app.get('/upload', function (req, res) {
     res.send("OK");
 });*/
 
+/*
 app.post('/upload', function (req, res) {
     var blobService = azure.createBlobService('boeingwepapp1','YqMF4F3rl76F/IhcRUXj1Ede1zHlSRHCtly/7BjB1cMAjsMBlksK3O8DPwFlIy0PfU/TiPBEDdvXGahZeeH4tQ==');  
     var form = new multiparty.Form();
@@ -74,8 +79,36 @@ app.post('/upload', function (req, res) {
     form.parse(req);
     res.setHeader('content-type', 'text/plain');
     res.send('OK');
-});	    
-	    
+});*/	    
+	  
+// Upload route.
+app.post('/upload', function(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        // `file` is the name of the <input> field of type `file`
+        var old_path = files.snapshot.path,
+            file_size = files.snapshot.size,
+            file_ext = files.snapshot.name.split('.').pop(),
+            index = old_path.lastIndexOf('/') + 1,
+            file_name = old_path.substr(index),
+            new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
+
+        fs.readFile(old_path, function(err, data) {
+            fs.writeFile(new_path, data, function(err) {
+                fs.unlink(old_path, function(err) {
+                    if (err) {
+                        res.status(500);
+                        res.json({'success': false});
+                    } else {
+                        res.status(200);
+                        res.json({'success': true});
+                    }
+                });
+            });
+        });
+    });
+});
+
 var port = process.env.PORT || 1337;
 
 app.listen(port,function(){
